d2221124d4a3ce1f547cd2182d1d1542df5ec4da
xuedongli <Abel_lixuedong@163.com>
2018-09-10 07:50:36 +0000

    Update database configuration

--- config/database.php
+++ config/database.php
@@ -1,5 +1,6 @@
 <?php
 
+$db_config = get_db_config();
 return [
 
     /*
@@ -13,7 +14,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    //'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => $db_config['connection'],
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +58,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $db_config['host'],
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            //'database' => env('DB_DATABASE', 'forge'),
+            'database' =>$db_config['database'],
+            //'username' => env('DB_USERNAME', 'forge'),
+            'username' =>$db_config['username'],
+            //'password' => env('DB_PASSWORD', ''),
+            'password' => $db_config['password'],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
